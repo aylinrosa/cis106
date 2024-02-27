6abfb61ea74de531822a037df7af22e2b6c711c9
aylinrosa <arosa31267@students.pccc.edu>
2024-02-26 23:03:32 -0500

    lab 3 finished

---
+++ labs/lab3/challenge_lab3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Author: Aylin Rosa
+echo "The current date is: "
+date -R
+echo ""
+echo "Some information about my system:"
+uname --all
+echo ""
+echo "RAM Information"
+free -h
+echo""
+echo "Challenge Question Complete"
+
+
+
